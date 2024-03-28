05bc1403d7fcb9daf2c0e736fc2e78b811be87ef
Przemek B <pprzemek89@gmail.com>
2024-03-28 19:25:33 +0100

    mierzenie czasu dodawania do listy
    work in progress, spróbuje to skrócić

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,54 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Random;
+import java.util.Scanner;
+
+import static java.lang.Thread.sleep;
 
 public class
 Main {
-    public static void main(String[] args) {
-        SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.add(10);
-        singleLinkedList.add(200);
-        singleLinkedList.add(2137);
-        singleLinkedList.add(10);
-        singleLinkedList.add(9);
-        singleLinkedList.add(340);
-        singleLinkedList.add(5);
-        singleLinkedList.remove(340);
-        System.out.println(singleLinkedList.search(2137));
-        singleLinkedList.display();
+    public static void main(String[] args) throws InterruptedException {
 
-        DoubleLinkedList list2 = new DoubleLinkedList();
-        list2.add(123000);
-        list2.add(4);
-        list2.add(65);
-        list2.add(3);
-        list2.remove(4);
-        list2.display();
+        Scanner s = new Scanner(System.in);
+        System.out.println("wciśnij enter aby zacząć");
+        s.nextLine();
+
+        int loop_n = 100000;
+
+        System.out.println("generowanie danych..");
+
+        int[] data = new int[loop_n];
+        Random rand = new Random();
+        for (int i = 0; i < loop_n; i++) {
+            data[i] = rand.nextInt(1000);
+        }
+
+        long end_time = System.currentTimeMillis();
+        long start_time = System.currentTimeMillis();
+        System.out.println(start_time);
+        int exc_time = 0;
+        System.out.println("\nWstawianie danych");
+
+        SingleLinkedList list_1 = new SingleLinkedList();
+        sleep(500);
+        start_time = System.currentTimeMillis();
+        for (int i = 0; i < loop_n; i++) {
+            list_1.add(data[i]);
+        }
+        end_time = System.currentTimeMillis();
+        exc_time= (int)(end_time-start_time);
+        System.out.println("tablica jednokierunkowa: "+exc_time+"ms");
+
+
+        DoubleLinkedList list_2 = new DoubleLinkedList();
+        sleep(500);
+        start_time = System.currentTimeMillis();
+        for (int i = 0; i < loop_n; i++) {
+            list_2.add(data[i]);
+        }
+        end_time = System.currentTimeMillis();
+        exc_time= (int)(end_time-start_time);
+        System.out.println("tablica dwukierunkowa: "+exc_time+"ms");
+
     }
-
-
 }
